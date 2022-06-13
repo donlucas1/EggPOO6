2c2d2bbc2a6f457238780c271f1ea240a5170890
donlucas1 <lucasenrio@gmail.com>
2022-06-13 18:44:57 -0300

    2do commit

--- POOEjercicio8/src/entidad/Cadena.java
+++ POOEjercicio8/src/entidad/Cadena.java
@@ -5,6 +5,8 @@
  */
 package entidad;
 
+import sun.security.util.Length;
+
 /**
  *
  * @author lucas
@@ -16,9 +18,9 @@ public class Cadena {
     public Cadena(){
     }
 
-    public Cadena(String frase, int longitud) {
+    public Cadena(String frase) {
         this.frase = frase;
-        this.longitud = longitud;
+        this.longitud = frase.length();
     }
 
     public String getFrase() {
@@ -36,7 +38,74 @@ public class Cadena {
     public void setLongitud(int longitud) {
         this.longitud = longitud;
     }
+//contabilizar la cantidad de vocales que tiene la frase ingresada
+    public int contarVocales(String frase) {
+        int i = 0;
 
+        for (int k = 0; k < frase.length(); k++) {
+            char ch = frase.charAt(k);
+            if ((((ch == 'a' || ch == 'e') || ch == 'i') || ch == 'o') || ch == 'u' ) {
+                i++;
+            }
+        }
+        return i;
+    }
     
+//invertir la frase ingresada y mostrarla por pantalla
+    public String invertirFrase(String frase) {
+        String fraseInvertida = "";
+        for (int k = frase.length() - 1; k >= 0; k--) {
+            char letra = frase.charAt(k);
+            fraseInvertida = fraseInvertida + letra;
+        }
+        return fraseInvertida;
+    }
+ //recibirá un carácter ingresado por el usuario y contabilizar cuántas veces se repite el carácter en la frase   
+    public int vecesRepetido(String letra) {
+        int repetido = 0;
+        char controlLetra = letra.charAt(0);
+        for (int k = 0; k < frase.length(); k++) {
+            char ch = frase.charAt(k);
+            if (ch == controlLetra) {
+                repetido++;
+            }
+        }
+        return repetido;
+    }
+    public String compararLongitud(String frase1){
+        String fraseComparada;
+        if (frase.length()==frase1.length()){
+            fraseComparada="tienen la misma longitud";
+        }else{
+                    fraseComparada="no tinen la misma longitud";
+                    }
+        
+    return fraseComparada;
+    }
+    public String unirFrases(String frase1, String frase2){
+        String fraseUnida;
+        fraseUnida= frase1+frase2;
+        return fraseUnida;
+    }
+    public String reemplazar(String frase, String caracter) {
+        String fraseReemplazada;
+        fraseReemplazada = frase.replace("a", caracter);
+        return fraseReemplazada;
+    }
     
+    public boolean contiene(String letra){
+        boolean contener=false;
+        char ch1 = letra.charAt(0);
+         for (int k = 0; k < frase.length(); k++) {
+            char ch = frase.charAt(k);
+            if (ch == ch1){
+                int i =0;
+                i++;
+            if (i>=1)
+            contener=true;
+    }
+    }    
+        return contener;
+                
+}
 }
--- POOEjercicio8/src/pooejercicio8/POOEjercicio8.java
+++ POOEjercicio8/src/pooejercicio8/POOEjercicio8.java
@@ -5,6 +5,9 @@
  */
 package pooejercicio8;
 
+import entidad.Cadena;
+import java.util.Scanner;
+
 /**
  *
  * @author lucas
@@ -15,7 +18,30 @@ public class POOEjercicio8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        String fase3;
+        Cadena C1 = new Cadena();
+        Cadena C2 = new Cadena();
+        System.out.println("Ingrese una frase");
+        C1.setFrase(leer.nextLine());
+        System.out.println("ingrese una frase para comparar");
+        C2.setFrase(leer.nextLine());
+        C1.setLongitud(C1.getFrase().length());
+        System.out.println(C1.invertirFrase(C1.getFrase()));
+        System.out.println("la frase tiene " + C1.contarVocales(C1.getFrase()) + " vocales " + C1.getFrase() + C1.getLongitud());
+        System.out.println("ingrese letra para ver cuantas veces se repitió");
+        String letra = leer.next();
+        leer.nextLine();
+        System.out.println("la letra aparece " + C1.vecesRepetido(letra));
+        System.out.println("las frases  " + C1.compararLongitud(C2.getFrase()));
+        System.out.println("ingrese una frase para unir");
+        System.out.println("las frases unidas son: " + C1.unirFrases(C1.getFrase(), fase3=leer.nextLine()));
+        String caracter;
+        System.out.println("escriba una letra para reemplazar la a de la frase");
+        System.out.println( "la frase nueva: "+ C1.reemplazar(C1.getFrase(), caracter=leer.next()));
+        System.out.println("ingrese una letra para ver si esta en la frase");
+        String letra1;
+        System.out.println("la letra esta en la frase: " + C1.contiene(letra1=leer.next()));
     }
     
 }
